b59489529611dc47d7bc9e9b28619f85ec577526
Alex Avo <grant5518@yandex.ru>
2022-01-08 01:31:57 +0300

    Add project files.

---
+++ Persistance/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Avo.SoftDev.Persistance
+{
+    public class Class1
+    {
+    }
+}
---
+++ SDTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Avo.SoftDev.SDTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
